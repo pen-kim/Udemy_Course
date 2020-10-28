f67b588ba1004f1060844451a9f5e78840aedfa2
pen-kim <professionalboolean@gmail.com>
2020-10-28 09:55:33 -0700

    Add files via upload
    string template literal

---
+++ section-15-exercise-20/index.js
@@ -0,0 +1,10 @@
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+ 
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1 + die2}.`
